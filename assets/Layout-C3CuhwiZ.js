a357b9511d867f31fb356032ba401c7c9d7daf38
SzyVsSi <szynid19@gmail.com>
2024-10-22 00:25:01 +0200

    Updates

---
+++ assets/Layout-C3CuhwiZ.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-DMpkUcN9.js";const r=({children:s})=>t.jsx("div",{className:"min-w-full min-h-screen bg-slate-300",children:s});export{r as L};
+//# sourceMappingURL=Layout-C3CuhwiZ.js.map
